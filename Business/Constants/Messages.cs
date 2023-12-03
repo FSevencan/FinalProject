fb9e280d137ecedb51071d249b64c3bf2c500f84
Fatih SEVENCAN <121979282+FSevencan@users.noreply.github.com>
2023-12-04 00:33:18 +0300

    -

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.Concrate;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Bir kategoriye sadece 10 adet ürün eklenebilir";
+        public static string ProductNameAlreadyExists = "Bu isimli ürün daha önce eklendi";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+
+        public static string? AuthorizationDenied = "Bu işlemi yapma yetkiniz yoktur";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+    }
+}
