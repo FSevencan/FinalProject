b1e1b9d0257b1ee4fe6fa5fa909448b9aad5b088
Fatih SEVENCAN <121979282+FSevencan@users.noreply.github.com>
2023-12-04 00:34:57 +0300

    -

---
+++ DataAccess/Concrate/EntityFramework/EfProductDal.cs
@@ -0,0 +1,35 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrate;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+
+namespace DataAccess.Concrate.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NortwindContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsStock = p.UnitsInStock
+                             };
+
+                return result.ToList();
+            }
+        }
+
+
+    }
+}
